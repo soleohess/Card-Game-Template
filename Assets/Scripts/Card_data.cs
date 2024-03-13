889e42857cf1b56806e09a56278048981bb7b9ad
soleohess <shess1272@student.pps.net>
2024-03-13 13:29:09 -0700

    Ansell fixed the cards
    the game works now (kinda)

--- Assets/Scripts/Card_data.cs
+++ Assets/Scripts/Card_data.cs
@@ -14,7 +14,7 @@ public class Card_data : ScriptableObject
 //    public int damage;
 //    public Sprite sprite;
 
-    public int suit; // Spades are 1, hearts are 2, diamonds are 3, clubs are 4, no jokers.
-    public int type;// 0 is a normal card, 1 is a jack, 2 is a queen, 3 is a king, 4 is an ace. This is used by the script.
-    public string symbol; // Represents symbol for sprite, jack is J, queen is Q, king is K, ace is A.
+    public Sprite suit; // Spades are 1, hearts are 2, diamonds are 3, clubs are 4, no jokers.
+    public int value;// 0 is a normal card, 1 is a jack, 2 is a queen, 3 is a king, 4 is an ace. This is used by the script.
+    //public string symbol; // Represents symbol for sprite, jack is J, queen is Q, king is K, ace is A.
 }
--- Assets/Scripts/PlayingCard.cs
+++ Assets/Scripts/PlayingCard.cs
@@ -8,20 +8,55 @@ public class PlayingCard : MonoBehaviour // This sprite draws card images.
 {
     public Card_data data;
 
-    public int suit;
-    public string symbol;
-    public Sprite sprite;
+    public TextMeshProUGUI Upper_Left_Symbol;
+    public TextMeshProUGUI Middle_Symbol;
+    public TextMeshProUGUI Lower_Right_Symbol;
+    public Image Upper_Right_Sprite;
+    public Image Lower_Left_Sprite;
     
-    public TextMeshProUGUI numberText;
-    public Image spriteImage;
-        
 
     // Start is called before the first frame update
     void Start()
     {
-        symbol = data.symbol;
-        numberText.text = symbol;
-        spriteImage.sprite = sprite;
+        if (data.value == 11)
+        {
+            Upper_Left_Symbol.text = "J";
+            Middle_Symbol.text = "J";
+            Lower_Right_Symbol.text = "J";
+        }
+        else if (data.value == 12)
+        {
+            Upper_Left_Symbol.text = "Q";
+            Middle_Symbol.text = "Q";
+            Lower_Right_Symbol.text = "Q";
+        }
+        else if (data.value == 13)
+        {
+            Upper_Left_Symbol.text = "K";
+            Middle_Symbol.text = "K";
+            Lower_Right_Symbol.text = "K";
+        }
+        else if (data.value == 14)
+        {
+            Upper_Left_Symbol.text = "A";
+            Middle_Symbol.text = "A";
+            Lower_Right_Symbol.text = "A";
+        }
+        else
+        {
+            Upper_Left_Symbol.text = data.value.ToString();
+            Middle_Symbol.text = data.value.ToString();
+            Lower_Right_Symbol.text = data.value.ToString();
+        }
+        
+    Upper_Right_Sprite.sprite = data.suit;
+    Lower_Left_Sprite.sprite = data.suit;
+        
+        
+
+        //symbol = data.symbol;
+        //numberText.text = symbol;
+        //spriteImage.sprite = sprite;
 
     }
 
